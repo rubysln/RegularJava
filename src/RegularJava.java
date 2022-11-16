0c00d104dd9bd2f0925750533e6d9964c038b426
rubysln <prostosekundu48@gmail.com>
2022-11-16 22:28:21 +0300

    first commit, start project

---
+++ src/RegularJava.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class RegularJava {
+    public boolean timeCheck(String line){
+        return false;
+    }
+    public boolean macAdressCheck(String line){
+        return false;
+    }
+    public boolean emailAdressCheck(String line){
+        return false;
+    }
+    public boolean passwordCheck(String line){
+        return false;
+    }
+}
